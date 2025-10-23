c21f4d5c8012b9222ada3797f826e9ced6c6765e
lucasMuner <lucas.muner0622@gmail.com>
2025-10-22 21:20:04 -0300

    Fix k6 script

--- setup.sh
+++ setup.sh
@@ -47,3 +47,10 @@ docker exec $WORKSPACE_ID chmod -R 775 storage bootstrap/cache
 for APP_ID in $(docker ps --filter "name=overload_app" --format "{{.ID}}"); do
     docker exec $APP_ID chown -R www-data:www-data storage bootstrap/cache
 done
+
+# Instalar k6 para testes de carga
+sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-get update
+sudo apt-get install k6
